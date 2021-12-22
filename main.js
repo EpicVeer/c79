d39c1149813c581904bac485ed5b44ceaa596205
EpicVeer <92925439+EpicVeer@users.noreply.github.com>
2021-12-22 15:52:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+studentarray=[]
+function Submit(){
+    var name1=document.getElementById("name1").value
+    var name2=document.getElementById("name2").value
+    var name3=document.getElementById("name3").value
+    var name4=document.getElementById("name4").value
+
+    studentarray.push(name1)
+    studentarray.push(name2)
+    studentarray.push(name3)
+    studentarray.push(name4)
+
+    document.getElementById("displayname").innerHTML=studentarray
+    document.getElementById("submit_button").style.display="none"
+    document.getElementById("sort_button").style.display="inline-block"
+}
+function sorting(){
+    studentarray.sort()
+    document.getElementById("displayname").innerHTML=studentarray
+    console.log(studentarray)
+}
